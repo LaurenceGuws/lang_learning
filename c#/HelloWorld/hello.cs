a85f0cbfb9b019f042f3fc1dd358063753078022
Laurence <lgouws@dearx.co.za>
2025-05-18 17:00:56 +0200

    Add Hello World + gitignore for the rest

---
+++ c#/HelloWorld/hello.cs
@@ -0,0 +1,11 @@
+//csc hello.cs      # Compiles to hello.exe (on Windows)
+//./hello           # Linux/macOS
+//hello.exe         # Windows
+
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ lua/hello_world/hello.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/env lua
+
+local function main()
+    print("Hello from standalone Lua!")
+    print("Current time: " .. os.date())
+end
+
+main()
+
